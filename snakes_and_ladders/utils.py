34ee697965b2ad2449b99bc40ac7bfddcd9bd350
Dan Nepejchal <dan.nepejchal@dhl.com>
2025-01-08 14:07:00 +0100

    Optimized roll_dice

--- snakes_and_ladders/utils.py
+++ snakes_and_ladders/utils.py
@@ -1,4 +1,17 @@
 import random
 
-def roll_dice():
-    return random.randint(1, 6)
+
+def roll_dice() -> int:
+    """
+    Rolls a die. If the result is 6, the player will roll again and sum the results.
+
+    Returns:
+        int: The total sum of the dice rolls.
+    """
+    total: int = 0
+    while True:
+        turn: int = random.randint(1, 6)
+        total += turn
+        if turn != 6:
+            break
+    return total
