a26e349ebaf517e482c46c55bcbb0fc3416475fd
Dan Nepejchal <dan.nepejchal@dhl.com>
2025-01-08 14:38:57 +0100

    Type hints, doc strings

--- snakes_and_ladders/board.py
+++ snakes_and_ladders/board.py
@@ -1,9 +1,42 @@
 class Board:
     def __init__(self):
-        self.snakes: dict[int, int] = {16: 6, 47: 26, 49: 11, 56: 53, 62: 19, 64: 60, 87: 24, 93: 73, 95: 75, 98: 78}
-        self.ladders: dict[int, int] = {1: 38, 4: 14, 9: 31, 21: 42, 28: 84, 36: 44, 51: 67, 71: 91, 80: 100}
+        """
+        Initializes the Board with predefined positions for snakes and ladders.
+        """
+        self.snakes: dict[int, int] = {
+            16: 6,
+            47: 26,
+            49: 11,
+            56: 53,
+            62: 19,
+            64: 60,
+            87: 24,
+            93: 73,
+            95: 75,
+            98: 78,
+        }
+        self.ladders: dict[int, int] = {
+            1: 38,
+            4: 14,
+            9: 31,
+            21: 42,
+            28: 84,
+            36: 44,
+            51: 67,
+            71: 91,
+            80: 100,
+        }
 
     def get_new_position(self, position: int) -> int:
+        """
+        Returns the new position of a player after encountering a snake or ladder.
+
+        Args:
+            position (int): The current position of the player.
+
+        Returns:
+            int: The new position of the player after moving up a ladder or down a snake.
+        """
         if position in self.snakes:
             return self.snakes[position]
         elif position in self.ladders:
--- snakes_and_ladders/game.py
+++ snakes_and_ladders/game.py
@@ -1,28 +1,40 @@
-import random
 from board import Board
 from player import Player
+from snakes_and_ladders.utils import roll_dice
+
 
 class Game:
     def __init__(self, num_players):
-        self.board = Board()
-        self.players = [Player(f"Hráč {i+1}") for i in range(num_players)]
-        self.current_player_index = 0
+        """
+        Initializes the Game with the specified number of players.
 
-    def roll_dice(self):
-        return random.randint(1, 6)
+        Args:
+            num_players (int): The number of players in the game.
+        """
+        self.board: Board = Board()
+        self.players: list[Player] = [Player(f"Hráč {i+1}") for i in range(num_players)]
+        self.current_player_index: int = 0
 
-    def play(self):
+    def play(self) -> None:
+        """
+        Starts and controls the flow of the game until a player wins.
+        """
         while True:
             current_player = self.players[self.current_player_index]
             print(f"{current_player.name} je na poli {current_player.position}")
-            roll = self.roll_dice()
+            roll = roll_dice()
             print(f"{current_player.name} hodil {roll}")
+            # TODO check the possibility of move
             current_player.move(roll)
-            current_player.set_position(self.board.get_new_position(current_player.position))
+            current_player.set_position(
+                self.board.get_new_position(current_player.position)
+            )
             print(f"{current_player.name} se posunul na pole {current_player.position}")
 
             if current_player.position == 100:
                 print(f"{current_player.name} vyhrál!")
                 break
 
-            self.current_player_index = (self.current_player_index + 1) % len(self.players)
+            self.current_player_index = (self.current_player_index + 1) % len(
+                self.players
+            )
