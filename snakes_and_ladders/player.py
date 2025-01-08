f444f7430073f73c84e2a3e8b57b81c465731611
Dan Nepejchal <dan.nepejchal@dhl.com>
2025-01-08 14:38:43 +0100

    Type hints, doc strings

--- snakes_and_ladders/player.py
+++ snakes_and_ladders/player.py
@@ -1,10 +1,28 @@
 class Player:
-    def __init__(self, name):
-        self.name = name
-        self.position = 0
+    def __init__(self, name: str):
+        """
+        Initializes a Player with a given name and starting position.
 
-    def move(self, steps):
+        Args:
+            name (str): The name of the player.
+        """
+        self.name: str = name
+        self.position: int = 0
+
+    def move(self, steps: int) -> None:
+        """
+        Moves the player forward by a given number of steps.
+
+        Args:
+            steps (int): The number of steps to move the player.
+        """
         self.position += steps
 
-    def set_position(self, position):
+    def set_position(self, position: int) -> None:
+        """
+        Sets the player's position to a specific value.
+
+        Args:
+            position (int): The new position of the player.
+        """
         self.position = position
