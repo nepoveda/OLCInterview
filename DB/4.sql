11c2c26b1a6e6e26c8d193fdfb8c1daadbd0e286
Dan Nepejchal <dan.nepejchal@dhl.com>
2025-01-12 14:02:48 +0100

    fourth sql task

---
+++ DB/4.sql
@@ -0,0 +1,22 @@
+SELECT
+    e.first_name,
+    e.last_name,
+    s.store_id,
+    EXTRACT(YEAR FROM r.rental_date) AS year,
+    COUNT(r.rental_id) AS rental_count
+FROM
+    rental r
+JOIN
+    staff e ON r.staff_id = e.staff_id
+JOIN
+    store s ON e.store_id = s.store_id
+GROUP BY
+    e.first_name,
+    e.last_name,
+    s.store_id,
+    EXTRACT(YEAR FROM r.rental_date)
+ORDER BY
+    e.last_name,
+    e.first_name,
+    s.store_id,
+    year;
